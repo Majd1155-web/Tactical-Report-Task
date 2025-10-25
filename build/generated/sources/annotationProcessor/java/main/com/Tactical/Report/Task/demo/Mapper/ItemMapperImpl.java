dd99b935aaeaf0b07e567cd8556d8daae8293054
MajdDaou <127030801+Majd1155-web@users.noreply.github.com>
2025-10-25 03:13:59 +0300

    setting gitignore

--- build/generated/sources/annotationProcessor/java/main/com/Tactical/Report/Task/demo/Mapper/ItemMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/Tactical/Report/Task/demo/Mapper/ItemMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-10-25T02:52:41+0300",
+    date = "2025-10-25T03:12:35+0300",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.14.3.jar, environment: Java 21.0.3 (Oracle Corporation)"
 )
 @Component
